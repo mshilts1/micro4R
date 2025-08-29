896702eb5a8d87c01beffd661913dfd66d76da88
mshilts1 <meghanshilts@gmail.com>
2025-08-29 15:43:53 -0500

    more updates to wrapper

--- R/dada2_wrapper.R
+++ R/dada2_wrapper.R
@@ -3,16 +3,17 @@
 #' @param where Path to your fastq files. Run `dada2_wrapper(path = "example")` if you want to run the example data.
 #' @param patternF Pattern of the forward/R1 reads
 #' @param patternR Pattern of the reverse/R2 reads
-#' @param ... Can alter defaults of dada2 functions if needed
+#' @param ... Can alter parameters of dada2 functions if needed
 #'
 #' @returns dada2 ASV table and taxonomy table
 #' @export
 #' @import dada2
 #' @import rlang
 #' @import utils
+#' @import grDevices
 #'
 #' @examples
-#' dada2_wrapper(where = "example")
+#' dada2_wrapper("example")
 dada2_wrapper <- function(where = NULL, patternF ="_R1_001.fastq.gz", patternR = "_R2_001.fastq.gz", ...){
   if(is.null(where)){
     where <- findUserCD()
@@ -58,10 +59,41 @@ dada2_wrapper <- function(where = NULL, patternF ="_R1_001.fastq.gz", patternR =
     cat("\nPlease note that, because the main goal of dada2::filterAndTrim is to *write* filtered fastq files to the user's computer, and you chose to use the 'example' data, this function was NOT run. The filtered fastq reads already exist as example data for this package, so this package won't try to write the any data to your computer. With your own data, dada2::filterAndTrim will actually run, and make take some time, depending on the size of your input data!\n\n")
   }
 
-  #print(filtFs)
 
   errF <- dada2::learnErrors(filtFs, multithread=TRUE)
   errR <- dada2::learnErrors(filtRs, multithread=TRUE)
+
+  if(where != "example"){ # create some dada2 figs
+
+    if (!dir.exists(sprintf("%s/dada2_figs", where))) {
+      # If it doesn't exist, create it
+      dir.create(sprintf("%s/dada2_figs", where), recursive = TRUE) # recursive = TRUE creates parent directories if needed
+    }
+
+    grDevices::pdf(sprintf("%s/dada2_figs/plotQualityProfileForwardReads_first10_samples.pdf", where))
+    print(dada2::plotQualityProfile(fnFs[1:10]))
+    grDevices::dev.off()
+    grDevices::pdf(sprintf("%s/dada2_figs/plotQualityProfileReverseReads_first10_samples.pdf", where))
+    print(dada2::plotQualityProfile(fnRs[1:10]))
+    grDevices::dev.off()
+
+    grDevices::pdf(sprintf("%s/dada2_figs/plotQualityProfileForwardReads_aggregate_all.pdf", where))
+    print(dada2::plotQualityProfile(fnFs, aggregate = TRUE))
+    grDevices::dev.off()
+    grDevices::pdf(sprintf("%s/dada2_figs/plotQualityProfileReverseReads_aggregate_all.pdf", where))
+    print(dada2::plotQualityProfile(fnRs, aggregate = TRUE))
+    grDevices::dev.off()
+  grDevices::pdf(sprintf("%s/dada2_figs/plotErrorsForward.pdf", where))
+  suppressWarnings(print(dada2::plotErrors(errF, nominalQ=TRUE)))
+  grDevices::dev.off()
+
+  grDevices::pdf(sprintf("%s/dada2_figs/plotErrorsReverse.pdf", where))
+  suppressWarnings(print(dada2::plotErrors(errR, nominalQ=TRUE)))
+  grDevices::dev.off()
+  }
+
+  derepFs <- derepFastq(filtFs, verbose=TRUE)
+  derepRs <- derepFastq(filtRs, verbose=TRUE)
 
 }
 
