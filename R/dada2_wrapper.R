a7f0c5c73dd109c77855414c89a37487850ee95a
mshilts1 <meghanshilts@gmail.com>
2025-08-29 13:56:45 -0500

    more updates to wrapper

--- R/dada2_wrapper.R
+++ R/dada2_wrapper.R
@@ -3,23 +3,25 @@
 #' @param where Path to your fastq files. Run `dada2_wrapper(path = "example")` if you want to run the example data.
 #' @param patternF Pattern of the forward/R1 reads
 #' @param patternR Pattern of the reverse/R2 reads
+#' @param ... Can alter defaults of dada2 functions if needed
 #'
 #' @returns dada2 ASV table and taxonomy table
 #' @export
 #' @import dada2
 #' @import rlang
+#' @import utils
 #'
 #' @examples
 #' dada2_wrapper(where = "example")
-dada2_wrapper <- function(where = NULL, patternF ="_R1_001.fastq.gz", patternR = "_R2_001.fastq.gz"){
+dada2_wrapper <- function(where = NULL, patternF ="_R1_001.fastq.gz", patternR = "_R2_001.fastq.gz", ...){
   if(is.null(where)){
     where <- findUserCD()
     print(sprintf("As no argument was provided for the 'path' of your fastq files, this wrapper will assume you want to work in your current directory, %s", where))
   }
 
   if(where == "example"){
-    fnFs <- system.file("extdata/example_fastq_files", package = "micro4R", mustWork = TRUE) %>% list.files("*_R1_001.fastq.gz")
-    fnRs <- system.file("extdata/example_fastq_files", package = "micro4R", mustWork = TRUE) %>% list.files("*_R2_001.fastq.gz")
+    fnFs <- system.file("extdata/f", package = "micro4R", mustWork = TRUE) %>% list.files("*_R1_001.fastq.gz")
+    fnRs <- system.file("extdata/f", package = "micro4R", mustWork = TRUE) %>% list.files("*_R2_001.fastq.gz")
   }
 
   if(where != "example") {
@@ -39,9 +41,9 @@ dada2_wrapper <- function(where = NULL, patternF ="_R1_001.fastq.gz", patternR =
   if(where != "example") {
   filtFs <- file.path(where, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
   filtRs <- file.path(where, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
-  out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(240,200), maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE, compress=TRUE, multithread=TRUE)
+  out <- dada2::filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(240,200), maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE, compress=TRUE, multithread=TRUE)
 
-  head(out)
+  utils::head(out)
   }
 
   if(where == "example") {
