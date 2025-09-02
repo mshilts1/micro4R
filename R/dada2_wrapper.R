2309b7f8d73ef23a9d646eb3a212d1c995712c29
mshilts1 <meghanshilts@gmail.com>
2025-09-02 07:41:36 -0500

    clean up user's environment after running example

--- R/dada2_wrapper.R
+++ R/dada2_wrapper.R
@@ -1,8 +1,8 @@
 #' dada2 Wrapper
 #'
 #' @param where Path to your fastq files. Run `dada2_wrapper(path = "example")` if you want to run the example data.
-#' @param patternF Pattern of the forward/R1 reads
-#' @param patternR Pattern of the reverse/R2 reads
+#' @param patternF Pattern of the forward/R1 reads. Default is Illumina standard.
+#' @param patternR Pattern of the reverse/R2 reads. Default is Illumina standard
 #' @param multi Should multithreading be done? Can use this to set to TRUE or FALSE for entire wrapper.
 #' @param ... Allows passing of arguments to nested functions
 #'
@@ -14,7 +14,7 @@
 #' @import grDevices
 #'
 #' @examples
-#' dada2_wrapper("example", db = "x")
+#' dada2_wrapper("example")
 dada2_wrapper <- function(where = NULL, patternF = "_R1_001.fastq.gz", patternR = "_R2_001.fastq.gz", multi = FALSE, ...) {
 
   passed_args <- list(...) # get a list of all arguments from user that we want/need to pass to nested functions. not doing anything with this yet. actual functionality to be added
@@ -24,12 +24,23 @@ dada2_wrapper <- function(where = NULL, patternF = "_R1_001.fastq.gz", patternR 
     print(sprintf("As no argument was provided for the 'path' of your fastq files, this wrapper will assume you want to work in your current directory, %s", where))
   }
 
+  if(where == "example"){
+    outdir <- tempdir()
+    print(sprintf("Because you're running the example, any output files go to a temporary directory, %s/dada2_out. To avoid cluttering your computer, this should all be deleted at the end of your R session.", outdir))
+  }
+  if(where != "example"){
+    if (!dir.exists(sprintf("%s/dada2_out/figs", where))) {
+      # If it doesn't exist, create it
+      dir.create(sprintf("%s/dada2_out/figs", where), recursive = TRUE) # recursive = TRUE creates parent directories if needed
+    }
+  }
+
   if (where == "example") {
     fnFs <- system.file("extdata/f", package = "micro4R", mustWork = TRUE) %>% list.files("*_R1_001.fastq.gz", full.names = TRUE)
     fnRs <- system.file("extdata/f", package = "micro4R", mustWork = TRUE) %>% list.files("*_R2_001.fastq.gz", full.names = TRUE)
 
-    filtFs <- system.file("extdata/f/filtered", package = "micro4R", mustWork = TRUE) %>% list.files("*_F_filt.fastq.gz", full.names = TRUE)
-    filtRs <- system.file("extdata/f/filtered", package = "micro4R", mustWork = TRUE) %>% list.files("*_R_filt.fastq.gz", full.names = TRUE)
+    #filtFs <- system.file("extdata/f/filtered", package = "micro4R", mustWork = TRUE) %>% list.files("*_F_filt.fastq.gz", full.names = TRUE)
+    #filtRs <- system.file("extdata/f/filtered", package = "micro4R", mustWork = TRUE) %>% list.files("*_R_filt.fastq.gz", full.names = TRUE)
   }
 
   if (where != "example") {
@@ -43,24 +54,23 @@ dada2_wrapper <- function(where = NULL, patternF = "_R1_001.fastq.gz", patternR 
     }
   }
 
-  sample.names <- gsub(patternF, "", basename(fnFs)) # create simplified sample names by stripped out the forward read pattern specified by user
-  # print(sample.names)
+  sample.names <- gsub(patternF, "", basename(fnFs)) # create simplified sample names by stripping out the forward read pattern specified by user. *should* match reverse reads, unless something really weird going on with file names.
 
   if (where != "example") {
-    filtFs <- file.path(where, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
-    filtRs <- file.path(where, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
+    filtFs <- file.path(where, "filtered/dada2_out", paste0(sample.names, "_F_filt.fastq.gz"))
+    filtRs <- file.path(where, "filtered/dada2_out", paste0(sample.names, "_R_filt.fastq.gz"))
     out <- dada2::filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen = c(240, 200), maxN = 0, maxEE = c(2, 2), truncQ = 2, rm.phix = TRUE, compress = TRUE, multithread = multi)
 
-    # why add the next three lines in? all reads negative controls and samples that amplified poorly with very few reads to start with may get entirely filtered out and cause a fatal error during the error rate learning step. so we need to make sure files still exist first.
+    # why add the next three lines in? sometimes samples with few reads to start with may get entirely filtered out and cause a fatal error during the error rate learning step. so we need to make sure files still exist first.
     exists <- file.exists(filtFs) & file.exists(filtRs)
     filtFs <- filtFs[exists]
     filtRs <- filtRs[exists]
-    # print(exists)
-    # return(filtFs)
   }
 
   if (where == "example") {
-    # cat("\nPlease note that, because the main goal of dada2::filterAndTrim is to *write* filtered fastq files to the user's computer, and you chose to use the 'example' data, this function was NOT run. The filtered fastq reads already exist as example data for this package, so this package won't try to write the any data to your computer. With your own data, dada2::filterAndTrim will actually run, and make take some time, depending on the size of your input data!\n\n")
+
+    filtFs <- file.path(outdir, "dada2_out/filtered", paste0(sample.names, "_F_filt.fastq.gz"))
+    filtRs <- file.path(outdir, "dada2_out/filtered", paste0(sample.names, "_R_filt.fastq.gz"))
 
     out <- dada2::filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen = c(240, 200), maxN = 0, maxEE = c(2, 2), truncQ = 2, rm.phix = TRUE, compress = TRUE, multithread = multi)
   }
@@ -132,11 +142,15 @@ dada2_wrapper <- function(where = NULL, patternF = "_R1_001.fastq.gz", patternR 
 
   seqtab.nochim.tibble <- as_tibble(seqtab, rownames = "SampleID")
 
-  if (!dir.exists(sprintf("%s/dada2_out", where))) {
-    # If it doesn't exist, create it
-    dir.create(sprintf("%s/dada2_out", where), recursive = TRUE) # recursive = TRUE creates parent directories if needed
+  if (where != "example") {
     write.csv(track, file=sprintf("%s/dada2_out/track.csv", where), row.names=FALSE)
     write.csv(seqtab.nochim.tibble, file=sprintf("%s/dada2_out/seqtab.nochim.csv", where), row.names=FALSE)
+  }
+
+  if (where == "example") {
+    write.csv(track, file=sprintf("%s/dada2_out/track.csv", outdir), row.names=FALSE)
+    write.csv(seqtab.nochim.tibble, file=sprintf("%s/dada2_out/seqtab.nochim.csv", outdir), row.names=FALSE)
+    unlink(outdir)
   }
 
 
