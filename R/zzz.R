ebe7ec481ce9310f48fdfc596a3c16beff3314a2
mshilts1 <meghanshilts@gmail.com>
2025-09-05 12:34:08 -0500

    styler cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("This is version ", packageVersion(pkgname),
-                        " of ", pkgname, ". CAUTION: This is package is under active development and its functions may change at any time, without warning! Please visit https://github.com/mshilts1/micro4R to see recent changes.")
+  packageStartupMessage(
+    "This is version ", packageVersion(pkgname),
+    " of ", pkgname, ". CAUTION: This is package is under active development and its functions may change at any time, without warning! Please visit https://github.com/mshilts1/micro4R to see recent changes."
+  )
 }
