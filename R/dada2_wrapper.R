1ed0b54acbc5e2517ea0a40400074d4f12cc692a
mshilts1 <meghanshilts@gmail.com>
2025-09-05 12:34:41 -0500

    adding option to add metadata file, for convenience

--- R/dada2_wrapper.R
+++ R/dada2_wrapper.R
@@ -1,15 +1,16 @@
 #' dada2 wrapper
 #'
 #' @param ... Allows passing of arguments to nested functions
+#' @param metadata OPTIONAL. Can pass your associated metadata object to this function so it packages it all up for you for downstream steps
 #'
-#' @returns dada2 ASV table and taxonomy table
+#' @returns a list of dada2 ASV table and taxonomy table (and metadata, optionally)
 #' @export
 #'
 #' @examples
 #' train <- "inst/extdata/db/EXAMPLE_silva_nr99_v138.2_toGenus_trainset.fa.gz"
 #' species <- "inst/extdata/db/EXAMPLE_silva_v138.2_assignSpecies.fa.gz"
 #' dada2_wrapper(where = "example", train = train, species = species)
-dada2_wrapper <- function(...) {
+dada2_wrapper <- function(metadata = NULL, ...) {
   passed_args <- list(...) # get a list of all arguments from user that we want/need to pass to nested functions. not doing anything with this yet. actual functionality to be added
 
   asvtable <- dada2_asvtable(...)
@@ -17,5 +18,11 @@ dada2_wrapper <- function(...) {
   taxa <- dada2_taxa(asvtable, ...)
 
   asvtable <- as_tibble(asvtable, rownames = "SampleID")
-  return(list("asvtable" = asvtable, "taxa" = taxa))
+
+  if (is.null(metadata)) {
+    return(list("asvtable" = asvtable, "taxa" = taxa))
+  }
+  if (!is.null(metadata)) {
+    return(list("asvtable" = asvtable, "taxa" = taxa, "metadata" = metadata))
+  }
 }
--- R/decontam_wrapper.R
+++ R/decontam_wrapper.R
@@ -17,39 +17,49 @@
 #' dada2_wrapper(where = "example", train = train, species = species)
 #' metadata <- example_metadata()
 #' decontam_wrapper(asvtable = asvtable, metadata = metadata)
-decontam_wrapper <- function(asvtable = NULL, taxa = NULL, metadata = NULL, ...){
-
-  if(tibble::is_tibble(metadata)){
-    metadata <- metadata %>% as.data.frame() %>% tibble::column_to_rownames(var = "SampleID")
+decontam_wrapper <- function(asvtable = NULL, taxa = NULL, metadata = NULL, ...) {
+  if (tibble::is_tibble(metadata)) {
+    metadata <- metadata %>%
+      as.data.frame() %>%
+      tibble::column_to_rownames(var = "SampleID")
     return(invisible(metadata))
   }
 
-  ps<-phyloseq::phyloseq(phyloseq::otu_table(asvtable, taxa_are_rows=FALSE), phyloseq::sample_data(metadata), phyloseq::tax_table(taxa))
+  ps <- phyloseq::phyloseq(phyloseq::otu_table(asvtable, taxa_are_rows = FALSE), phyloseq::sample_data(metadata), phyloseq::tax_table(taxa))
 
   df <- as.data.frame(sample_data(ps))
   LibrarySize <- NULL
   df$LibrarySize <- sample_sums(ps)
 
-  where <- tempdir()  #getwd()
-  #if (where != "example" & where != "inst/extdata/f") {
-    if (!dir.exists(sprintf("%s/dada2_out/figs", where))) {
-      # If it doesn't exist, create it
-      dir.create(sprintf("%s/dada2_out/figs", where), recursive = TRUE) # recursive = TRUE creates parent directories if needed
-    }
-  #}
+  where <- tempdir() # getwd()
+  # if (where != "example" & where != "inst/extdata/f") {
+  if (!dir.exists(sprintf("%s/dada2_out/figs", where))) {
+    # If it doesn't exist, create it
+    dir.create(sprintf("%s/dada2_out/figs", where), recursive = TRUE) # recursive = TRUE creates parent directories if needed
+  }
+  # }
 
-  df <- df[order(df$LibrarySize),]
+  df <- df[order(df$LibrarySize), ]
   Index <- NULL
   df$Index <- seq(nrow(df))
   grDevices::pdf(sprintf("%s/dada2_out/figs/Library_sizes_pre_decontam.pdf", where))
-  print(ggplot2::ggplot(data=df, ggplot2::aes(x=.data[Index], y=.data[LibrarySize], color=.data$neg)) + ggplot2::geom_point())
+  print(ggplot2::ggplot(data = df, ggplot2::aes(x = .data[Index], y = .data[LibrarySize], color = .data$neg)) +
+    ggplot2::geom_point())
   grDevices::dev.off()
 
   contamdf.prev <- decontam::isContaminant(ps, neg = "neg")
-  table(contamdf.prev$contaminant)
-  head(which(contamdf.prev$contaminant))
 
-  ps.pa <- transform_sample_counts(ps, function(abund) 1*(abund>0))
+  if (all(!contamdf.prev$contaminant)) {
+    print("No contaminants were detected. Existing function.")
+  }
 
-  ps.pa.neg <- prune_samples(sample_data(ps.pa)$neg == "yes", ps.pa)
+  if (any(contamdf.prev$contaminant)) {
+    table(contamdf.prev$contaminant)
+    head(which(contamdf.prev$contaminant))
+
+    ps.pa <- transform_sample_counts(ps, function(abund) 1 * (abund > 0))
+
+    ps.pa.neg <- prune_samples(sample_data(ps.pa)$neg == "yes", ps.pa)
+    print(ps.pa.neg)
+  }
 }
