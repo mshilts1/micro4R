1e8177fded2c0fc5cb6b5371bf3adda62bb9373a
mshilts1 <meghanshilts@gmail.com>
2025-10-02 12:56:52 -0500

    initial add of full wrapper

---
+++ R/dada2_decontam_wrapper.R
@@ -0,0 +1,6 @@
+dada2_decontam_wrapper <- function(...) {
+
+  dada2_out <- dada2_wrapper(...)
+
+  decontam_out <- decontam_wrapper(asvtable = converter(dada2_out$asvtable), taxa = converter(dada2_out$taxa, id = "ASV"), metadata = dada2_out$metadata, ...)
+}
