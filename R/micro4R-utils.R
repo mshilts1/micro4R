0ba1c9b6ee0a7b4a07098d56f86de103ba4ccf05
mshilts1 <meghanshilts@gmail.com>
2025-10-17 10:10:01 -0500

    trying to identify cause of failure

--- R/micro4R-utils.R
+++ R/micro4R-utils.R
@@ -275,6 +275,9 @@ converter <- function(x = NULL, out = "matrix", id = "SampleID") {
     }
 
     if (out == "tibble"  & tibble::has_rownames(x) == TRUE) {
+      if(id %in% names(x)){
+        stop(sprintf("'id' column %s was already found in your input object, but you have rownames. Your input colnames are %s", id, names(x)))
+      }
       x <- rownames_to_column(x, var = id)
       x <- tibble::as_tibble(x)
       return(invisible(x))
