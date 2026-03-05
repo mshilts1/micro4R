dd145da4bc3e4370438e373dc389414e98641f16
Meghan Shilts <meghanshilts@Meghans-MacBook-Air.local>
2026-03-05 11:54:02 +1100

    cleaning up dada2 wrapper

--- R/dada2_wrapper.R
+++ R/dada2_wrapper.R
@@ -1,58 +1,47 @@
 #' dada2 wrapper
 #'
 #' @param ... Allows passing of arguments to nested functions
-#' @param metadata OPTIONAL. Can pass your associated metadata object to this function so it packages it all up for you for downstream steps
+#' @param metadata OPTIONAL. Pass associated metadata to include in output list.
 #' @param example Set to TRUE to run an example
-#' @param listargs List passed arguments. Using only for troubleshooting during development; shouldn't have any use to end user
-#' @param full.wrapper TRUE by default, but must be sent to FALSE when using dada2_decontam_wrapper as otherwise metadata objects collide
-#' @param log Produce a log file
+#' @param listargs List passed arguments (dev/troubleshooting)
+#' @param full.wrapper FALSE by default; must be FALSE when using
+#'   dada2_decontam_wrapper as otherwise metadata objects collide
+#' @param log Produce a log file (currently not wired through)
 #'
-#' @returns a list of dada2 ASV table and taxonomy table (and metadata, optionally)
+#' @returns a list with ASV table + taxonomy table (+ metadata, optionally)
 #' @export
-#'
 #' @examples
 #' dada2_wrapper(example = TRUE)
-dada2_wrapper <- function(example = FALSE, metadata = NULL, listargs = FALSE, full.wrapper = FALSE, log = TRUE, ...) {
-  passed_args <- list(...) # get a list of all arguments from user that we want/need to pass to nested functions. not doing anything with this yet. actual functionality to be added
+dada2_wrapper <- function(example = FALSE,
+                          metadata = NULL,
+                          listargs = FALSE,
+                          full.wrapper = FALSE,
+                          log = TRUE,
+                          ...) {
 
+  passed_args <- list(...)
 
-  if (listargs == TRUE) {
+  if (isTRUE(listargs)) {
     print(passed_args)
     return(passed_args)
   }
 
-  if (listargs == FALSE) {
-    if (example == TRUE & full.wrapper == FALSE) {
-      metadata <- example_metadata()
-    }
-
-    if (example == TRUE) {
-      asvtable <- dada2_asvtable(example = TRUE, ...)
-    }
-    if (example == TRUE) {
-      taxa <- dada2_taxa(example = TRUE, ...)
-    }
-
-    if (example == FALSE) {
-      asvtable <- dada2_asvtable(example = FALSE, ...) # logfile = log, ...)
-      # return(invisible(asvtable))
-    }
-
-    if (example == FALSE) {
-      taxa <- dada2_taxa(asvtable = asvtable, example = FALSE, ...)
-      # return(invisible(taxa))
-    }
-
-    # asvtable <- converter(asvtable, out = "tibble")
-    # taxa <- as_tibble(taxa, rownames = "ASV")
-
-    if (is.null(metadata) | full.wrapper == TRUE) {
-      checkAll(asvtable = asvtable, taxa = taxa)
-      return(list("asvtable" = asvtable, "taxa" = taxa))
-    }
-    if (!is.null(metadata) & full.wrapper == FALSE) {
-      checkAll(asvtable = asvtable, taxa = taxa, metadata = metadata)
-      return(list("asvtable" = asvtable, "taxa" = taxa, "metadata" = metadata))
-    }
+  # If running the example and we are NOT in the "full wrapper" scenario,
+  # attach example metadata unless user supplied metadata explicitly.
+  if (isTRUE(example) && !isTRUE(full.wrapper) && is.null(metadata)) {
+    metadata <- example_metadata()
   }
+
+  # Build core objects
+  asvtable <- dada2_asvtable(example = isTRUE(example), ...)
+  taxa <- dada2_taxa(asvtable = asvtable, example = isTRUE(example), ...)
+
+  # Validate
+  if (is.null(metadata) || isTRUE(full.wrapper)) {
+    checkAll(asvtable = asvtable, taxa = taxa)
+    return(list(asvtable = asvtable, taxa = taxa))
+  }
+
+  checkAll(asvtable = asvtable, taxa = taxa, metadata = metadata)
+  list(asvtable = asvtable, taxa = taxa, metadata = metadata)
 }
